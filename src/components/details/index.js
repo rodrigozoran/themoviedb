876f85b0849ba53c07d2037544d24f2b5eaef3a7
rodrigozoran <rodrigozoran@gmail.com>
2023-06-28 12:39:14 -0300

    Detalhes dos filmes

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import config from '../../config/config'
 
 import '../details/styles.css'
 
@@ -7,7 +8,41 @@ export default function DetailsPage() {
 
   const {id} = useParams();
 
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    fetch((config.get_details_movies + id + "?api_key=" + config.apiKey + config.lang + config.pages))
+    .then(response => response.json())
+    .then(data => {
+
+      const { title, poster_path, release_date, overview, tagline } = data;
+
+      const movie = {
+        id,
+        title: title,
+        sinopse: overview,
+        imagem_filme: config.img_url_base+poster_path,
+        releaseDate: release_date,
+        tagline: tagline
+      }
+      setMovie(movie)
+      console.log(data)
+    })
+    .catch(err => console.error(err))
+  }, [id])
+
   return (
-    <div>Funcionando</div>
+    <div className='container-details'>
+      <div className='movie-details'>
+        <img src={movie.imagem_filme} alt={movie.sinopse}></img>
+        <div className='detailsMovie1'>
+          <h1>{movie.title}</h1>
+          <p>{movie.tagline}</p>
+          <span className='sinopse'>Sinopse: {movie.sinopse}</span>
+          <span className='lancamento'>Lançamento: {movie.releaseDate}</span>
+          <Link to="/"><button>Voltar</button></Link>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,8 @@ const config = {
     apiKey: '69921a77e950ed50871616a93722bc82',
     lang: '&language=pt-BR',
     pages: "&page=1",
-    img_url_base: 'https://image.tmdb.org/t/p/w500'
+    img_url_base: 'https://image.tmdb.org/t/p/w500',
+    get_details_movies: `https://api.themoviedb.org/3/movie/`
 }
 
 export default config;
