528c1ce197041722797b9394b77dcdd2edee6bbb
Rodrigo Garcez <69591377+rodrigozoran@users.noreply.github.com>
2023-12-20 01:00:53 -0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     mostPopularUrl: 'https://api.themoviedb.org/3/movie/popular?api_key=',
-    apiKey: '69921a77e950ed50871616a93722bc82',
+    apiKey: '7f62a25968187178244b709e2729ecd6',
     lang: '&language=pt-BR',
     pages: "&page=1",
     img_url_base: 'https://image.tmdb.org/t/p/w500',
